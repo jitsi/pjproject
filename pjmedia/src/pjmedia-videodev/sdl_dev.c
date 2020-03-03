6fa84091a14cabe9d7e4c7fe7e7f9dda09d35cdf
Saúl Ibarra Corretgé <s@saghul.net>
2020-03-03 16:45:28 +0100

    sdl: fix resizing when in full-screen mode

--- pjmedia/src/pjmedia-videodev/sdl_dev.c
+++ pjmedia/src/pjmedia-videodev/sdl_dev.c
@@ -1198,6 +1198,7 @@ static pj_status_t set_cap(void *data)
 
         return status;
     } else if (cap == PJMEDIA_VID_DEV_CAP_OUTPUT_RESIZE) {
+<<<<<<< HEAD
         pjmedia_rect_size *new_size = (pjmedia_rect_size *)pval;
         Uint32 flag = SDL_GetWindowFlags(strm->window);
         pj_status_t status;
@@ -1217,6 +1218,27 @@ static pj_status_t set_cap(void *data)
             SDL_SetWindowFullscreen(strm->window, SDL_WINDOW_FULLSCREEN_DESKTOP);
 
         return status;
+=======
+	pjmedia_rect_size *new_size = (pjmedia_rect_size *)pval;
+	Uint32 flag = SDL_GetWindowFlags(strm->window);
+	pj_status_t status;
+
+	/**
+	 * Exit full-screen if engaged, since resizing while in full-screen is
+         * not supported.
+	 */
+	if (flag & SDL_WINDOW_FULLSCREEN_DESKTOP)
+	    SDL_SetWindowFullscreen(strm->window, 0);
+
+	SDL_SetWindowSize(strm->window, new_size->w, new_size->h);
+	status = resize_disp(strm, new_size);
+
+	/* Restore full-screen if it was engaged. */
+	if (flag & SDL_WINDOW_FULLSCREEN_DESKTOP)
+	    SDL_SetWindowFullscreen(strm->window, SDL_WINDOW_FULLSCREEN_DESKTOP);
+
+	return status;
+>>>>>>> 312ab0f75 (sdl: fix resizing when in full-screen mode)
     } else if (cap == PJMEDIA_VID_DEV_CAP_OUTPUT_WINDOW) {
         pjmedia_vid_dev_hwnd *hwnd = (pjmedia_vid_dev_hwnd*)pval;
         pj_status_t status = PJ_SUCCESS;
