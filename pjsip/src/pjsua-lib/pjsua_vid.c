bbe8177ddcc6bace78244dfcc714768c69a6bdb7
Saúl Ibarra Corretgé <s@saghul.net>
2020-03-04 14:30:11 +0100

    pjsua: automagically set remote video window to full-screen
    (cherry picked from commit 1b67e3c9d48571ff4b7ef67e89abba663cbaf4a5)

--- pjsip/src/pjsua-lib/pjsua_vid.c
+++ pjsip/src/pjsua-lib/pjsua_vid.c
@@ -885,6 +885,8 @@ static pj_status_t create_vid_win(pjsua_vid_win_type type,
         vp_param.vidparam.window_hide = !show;
         vp_param.vidparam.flags |= PJMEDIA_VID_DEV_CAP_OUTPUT_WINDOW_FLAGS;
         vp_param.vidparam.window_flags = wnd_flags;
+        vp_param.vidparam.flags |= PJMEDIA_VID_DEV_CAP_OUTPUT_FULLSCREEN;
+        vp_param.vidparam.window_fullscreen = PJMEDIA_VID_DEV_FULLSCREEN_DESKTOP;
         if (wnd) {
             vp_param.vidparam.flags |= PJMEDIA_VID_DEV_CAP_OUTPUT_WINDOW;
             vp_param.vidparam.window = *wnd;
